a37c9cb2cad210bd8436657b82ef0c122b133684
Marija Petrovic <mp20190194@student.fon.bg.ac.rs>
2023-02-09 18:52:48 +0100

    modeli, kozmeticar, musterija, salon

---
+++ app/Models/Kozmeticar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kozmeticar extends Model
+{
+    use HasFactory;
+     public function salon()
+    {
+        return $this->belongsTo(Salon::class);
+    }
+
+    public function musterije()
+    {
+        return $this->hasMany(Musterija::class);
+    }
+}
